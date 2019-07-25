23b0ecfa93579ab67b28e61dafc14c9ee82aa5f1
preethasiri <siripreetha@gmail.com>
2019-07-26 09:41:24 +1000

    with the correct filenaming

--- Scripts/Datadownload.R
+++ Scripts/Datadownload.R
@@ -1,5 +1,6 @@
 # Download datasets
 download.file(url = "https://mq-software-carpentry.github.io/R-git-for-research/data/SAFI_messy.xlsx", destfile = "./data/SAFI_messy.xlsx", mode = "wb")
-download.file(url = "https://mq-software-carpentry.github.io/R-git-for-research/data/SAFI_clean.csv", destfile = "./data/SAFI_clean.csv")
+download.file(url = "https://mq-software-carpentry.github.io/R-git-for-research/data/SAFI_clean.csv", 
+              destfile = "./data/SAFI_clean.csv")
 download.file(url = "https://mq-software-carpentry.github.io/R-git-for-research/data/SAFI_dates.xlsx", destfile = "./data/SAFI_dates.xlsx", mode = "wb")
 download.file(url = "https://mq-software-carpentry.github.io/R-git-for-research/data/SAFI_openrefine.csv", destfile = "./data/SAFI_openrefine.csv", mode = "wb")
---
+++ Scripts/StartingwithData.R
@@ -0,0 +1,4 @@
+library(tidyverse)
+interviews <- read_csv("data/SAFI_clean.csv", na="NULL") 
+
+
