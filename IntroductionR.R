87b1b94c43758c1d6b845da664163873d3f78f86
preethasiri <siripreetha@gmail.com>
2019-07-25 15:48:38 +1000

    chnages with functions

--- IntroductionR.R
+++ IntroductionR.R
@@ -1,4 +1,16 @@
 #Load required packages
 #install.packages('tidyverse")
 print("Hello,World")
+
 library(tidyverse)
+(area_hectares <- 1.0) # land area in hectares 
+1.0 ->area_hectares
+2.47*area_hectares
+
+# area_hectares <- 2.5  # land area in hectares 
+# area_acres <- 2.47 * area_hectares #convert to acres
+# area_acres #print
+length <-3
+width <-3
+area <-length * width
+area
