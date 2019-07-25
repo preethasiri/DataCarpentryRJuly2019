87e39ee342a93bee9385b0afed738b1eb184e722
preethasiri <siripreetha@gmail.com>
2019-07-25 16:07:30 +1000

    functions

--- IntroductionR.R
+++ IntroductionR.R
@@ -14,3 +14,26 @@ length <-3
 width <-3
 area <-length * width
 area
+# b <-sqrt(x=a)
+
+round(3.14159,digits=2)
+no_members <-c(3,7,10,6)
+length(no_members)
+class(no_members)
+
+respondent_wall_type<-c("muddaub", "burntbricks", "sunbricks")
+length(respondent_wall_type)
+class(respondent_wall_type)
+str(respondent_wall_type)
+
+possessions <-c("bicycle", "radio","television")
+possessions <- c(possessions, "mobile_phone") #add another element to vector  
+possessions <-c("car", possessions)  
+
+num_char <- c(1,2,3,"a") 
+num_logical <-c (1,2,3,FALSE)
+char_logical <-c("a", "b", "c", TRUE)
+tricky <-c(1,2,3,"4")
+combined_logical <-c(num_logical,char_logical)
+
+
