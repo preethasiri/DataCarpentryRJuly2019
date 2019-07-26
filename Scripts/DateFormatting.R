0e4495fe0dabab91349d04e3c37b5671c5577d99
preethasiri <siripreetha@gmail.com>
2019-07-26 12:25:48 +1000

    with the filter and select or pipe functions

---
+++ Scripts/DateFormatting.R
@@ -0,0 +1,16 @@
+library(dplyr)
+library(tidyr)
+
+interviews <- read_csv("data/SAFI_clean.csv", na="NULL")
+
+select(interviews, village, no_membrs, years_liv)
+filter(interviews,village == "God")
+filter(interviews, village ==  "God",no_membrs > 6)
+select_cols <-select(interviews,village, no_membrs, years_liv)
+god_data <- filter (select_cols, village == "God", no_membrs > 6)
+interviews %>% 
+  select(village, no_membrs, years_liv) %>%filter(village == "God", no_membrs >6)
+#Exercise 
+interviews %>% 
+  filter(memb_assoc =="yes") %>%
+  select (affect_conflicts, liv_count, no_meals)
