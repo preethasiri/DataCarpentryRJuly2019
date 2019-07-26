3582ee04febc5cfdb0a5d6bfa02e4c52528148a8
preethasiri <siripreetha@gmail.com>
2019-07-26 14:40:27 +1000

    interviews spread version

--- Scripts/DateFormatting.R
+++ Scripts/DateFormatting.R
@@ -15,4 +15,41 @@ interviews %>%
   filter(memb_assoc =="yes") %>%
   select (affect_conflicts, liv_count, no_meals)
 
-interviews_ppl_room<- intreviews %>% mutate(people_per_room = no_membrs / room) 
+interviews_ppl_room<- interviews %>% mutate(people_per_room = no_membrs / rooms) 
+interviews %>% mutate(people_per_room = no_membrs / rooms)
+
+view(interviews_ppl_room)
+total_meals <- interviews %>% mutate(total_meals = no_membrs * no_meals) %>% 
+  filter(total_meals > 20)%>%
+  select(village,total_meals)
+
+interviews %>%
+ filter(!is.na(memb_assoc)) %>%
+ group_by(village, memb_assoc) %>%
+ summarize(mean_no_membrs = mean(no_membrs),
+           min_membrs = min(no_membrs)) %>%
+
+arrange(desc(min_membrs))
+
+interviews%>% count(village,sort = TRUE)
+
+interviews %>% 
+  group_by(village)  %>% 
+  summarize (mean = mean(no_membrs),
+    max = max(no_membrs),
+    min=min(no_membrs),
+    n= n()
+  )
+ 
+interviews_spread <-interviews %>%mutate(wall_type_logical = TRUE)%>%
+  spread(key = respondent_wall_type, value = wall_type_logical, fill = FALSE)
+
+view(interviews_spread)
+
+interviews_gather <- interviews_spread %>%
+  gather(key ="respondent_wall_type", value = "wall_type_logical", burntbricks:sunbricks)
+filter(wall_type_logical)%>%
+  select(-wall_type_logical)
+
+view(interviews_gather)
+  
