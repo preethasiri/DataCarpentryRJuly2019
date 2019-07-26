6d4bd5ca244974eabc481cdd1a547655086a44ce
preethasiri <siripreetha@gmail.com>
2019-07-26 10:37:39 +1000

    subsetting

--- Scripts/StartingwithData.R
+++ Scripts/StartingwithData.R
@@ -1,4 +1,52 @@
 library(tidyverse)
 interviews <- read_csv("data/SAFI_clean.csv", na="NULL") 
+view(interviews)
+dim(interviews)
+nrow(interviews)
+ncol(interviews)
+
+dim(interviews)[1] == nrow(interviews)
 
 
+
+interviews
+head(as.data.frame(interviews))
+tail(as.data.frame(interviews))
+
+names(interviews)
+str(interviews)
+summary(interviews)
+
+
+interviews [1,1]
+interviews[1,6]
+interviews[ ,1]
+
+interviews[1]
+1:3
+interviews[1:3, 7]
+interviews[3, ]
+interviews [1:6, ]
+
+
+interviews_no_id <-interviews[ , -1]
+interviews_no_id
+interviews
+
+#Exercise 
+#1
+interviews_100 <-interviews[100, ]
+
+#2
+nrow(interviews)
+nrows <- nrow(interviews)
+interviews_last <- interviews[nrows, ]
+
+tail(interviews, n=1)
+
+#3
+middle_idx <- ceiling(n_rows/2)
+interveiws[middle_idx, ]
+
+#4
+interviews_head <-interviews[-(7:nrows), ]
