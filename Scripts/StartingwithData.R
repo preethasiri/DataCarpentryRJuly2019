1182544c1db4c7162fe8b324680473bf5f98ec22
preethasiri <siripreetha@gmail.com>
2019-07-26 11:23:04 +1000

    changes with factors part 2

--- Scripts/StartingwithData.R
+++ Scripts/StartingwithData.R
@@ -50,3 +50,34 @@ interveiws[middle_idx, ]
 
 #4
 interviews_head <-interviews[-(7:nrows), ]
+
+floor_type <- factor(c("earth", "cement", "cement", "earth"))
+levels(floor_type)
+nlevels(floor_type)
+
+
+
+##Reordering
+floor_type <- factor(c("earth", "cement", "cement", "earth"), 
+                     levels = c("earth", "cement"))
+
+levels(floor_type)[2] <- "brick"
+levels(floor_type)
+floor_type
+
+
+
+as.character(floor_type)
+
+
+
+year_fct <- factor(c(1990, 1983, 1977, 1998,1990 ))
+as.numeric(year_fct)
+as.numeric(as.character(year_fct)) 
+
+
+year_numb <- as.numeric(levels(year_fct)) [year_fct]
+table(year_nmb)
+table(year_fct)
+summary(year_fct)
+
