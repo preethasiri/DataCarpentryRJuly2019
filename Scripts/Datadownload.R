dc03bc5d281ca3d4768198dd5af715fda6d4c89d
preethasiri <siripreetha@gmail.com>
2019-07-26 18:58:43 +1000

    end of unitraining 2019sem2start

--- Scripts/Datadownload.R
+++ Scripts/Datadownload.R
@@ -4,3 +4,7 @@ download.file(url = "https://mq-software-carpentry.github.io/R-git-for-research/
               destfile = "./data/SAFI_clean.csv")
 download.file(url = "https://mq-software-carpentry.github.io/R-git-for-research/data/SAFI_dates.xlsx", destfile = "./data/SAFI_dates.xlsx", mode = "wb")
 download.file(url = "https://mq-software-carpentry.github.io/R-git-for-research/data/SAFI_openrefine.csv", destfile = "./data/SAFI_openrefine.csv", mode = "wb")
+
+download.file(url = "https://mq-software-carpentry.github.io/R-git-for-research/data/Interviews_plotting.rds", destfile = "./Data/Interviews_plotting.rds")
+
+
---
+++ Scripts/GGPlot2.R
@@ -0,0 +1,48 @@
+library(ggplot2)
+interviews_plotting 
+
+## ggplot(data = <DATA>,
+## mapping = aes(<MAPPINGS>)) + <GEOM_FUNCTION> ()
+
+
+ggplot(data= Interviews_plotting, aes(x=no_membrs, y=number_items)) +
+  geom_point(alpha=0.4)
+
+ggplot(data=Interviews_plotting, aes(x=no_members, y=number_items))
+
+ggplot(data = Interviews_plotting, aes(x = no_membrs, y = number_items)) +
+  geom_point()
+
+ggplot(data = Interviews_plotting, aes(x = no_membrs, y = number_items)) +
+  geom_jitter(alpha = 0.5, color = "blue")
+
+ggplot(data = Interviews_plotting, aes(x = respondent_wall_type, y = rooms)) +
+  geom_boxplot(alpha = 0) +
+  geom_jitter(alpha = 0.5, color = "tomato")
+
+ggplot(data = Interviews_plotting, aes(x = respondent_wall_type, y = rooms)) +
+  geom_boxplot(alpha = 0) +
+  geom_jitter(alpha = 0.5, color = "tomato", width = 0.2)
+
+
+ggplot(data = Interviews_plotting, aes(x = respondent_wall_type, y = rooms))geom_jitter(alpha = 0.5, color = "tomato", width = 0.2) + geom_boxplot(alpha = 0)
+
+
+ggplot(data = Interviews_plotting,aes(x=respondent_wall_type, y=liv_count, color=memb_assoc))+
+  geom_jitter(alpha=0.5, width =0.2, aes(color=memb_assoc))+
+  geom_boxplot(alpha=0)
+
+ggplot(data = Interviews_plotting,aes(x=respondent_wall_type, y=liv_count, color=memb_assoc))+
+  geom_jitter(alpha=0.5, width =0.2, aes(color=memb_assoc, height =0.2))+
+  geom_boxplot(alpha=0)
+
+ggplot(Interviews_plotting, aes (x=respondent_wall_type)) +
+geom_bar(aes(fill=village), position = "dodge") 
+labs (X ="wall type",y="Count")+
+facet_grid(memb_assoc~village)+theme (axis.text.x=element_text(angle = 9))
+
+
+
+
+
+
